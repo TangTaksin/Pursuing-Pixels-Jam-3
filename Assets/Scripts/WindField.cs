5099e2b5089fb6b6bda54929031ad7f28bb6960b
ResNeedle <110387872+ResNeedle@users.noreply.github.com>
2024-06-21 18:11:02 +0700

    Balance Bar & "Fail State"
    - placeholder animations

--- Assets/Scripts/WindField.cs
+++ Assets/Scripts/WindField.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class WindField : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("possive is left, negative is right")]
+    public float windStrenght;
+    TightRopeWalker walker;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        walker = other.GetComponent<TightRopeWalker>();
+        walker.SetInfluence(windStrenght);
+
+        print("Entered Wind Zone");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        walker.SetInfluence(0);
+        walker = null;
+
+        print("Exited Wind Zone");
     }
 }
